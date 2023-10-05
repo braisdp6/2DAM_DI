b1e15b6bf5315d31fbbc1b8f60e0fe2105567cb6
braisdp6 <braisdominguezpuga6@gmail.com>
2023-10-05 14:20:25 +0200

    act

--- TEMA 1/Eventos.py
+++ TEMA 1/Eventos.py
@@ -1,7 +1,20 @@
-import Var
+import Var,sys
 class Eventos():
     def salir(self):
         try:
+            sys.exit()
             pass
         except Exception as error:
             print(error, "en modulo eventos")
+
+    def abrirCalendar(self):
+        try:
+            Var.calendar.show()
+        except Exception as error:
+            print("Error en abrir calendar", error)
+
+ def acerdade():
+     try:
+         pass
+     except Exception as error:
+         print("Error abrir ventana acerca de", error)
--- TEMA 1/main.py
+++ TEMA 1/main.py
@@ -1,18 +1,34 @@
-import Eventos
+from datetime import datetime
+from VentanaCalendario import Ui_ventanaCalendario
 from MainWindow import *
-import sys,Var
+import sys,Var,Eventos
 
+
+
+class Calendar(QtWidgets.QDialog):
+    def __init__(self):
+        super(Calendar, self).__init__()
+        Var.calendar = Ui_ventanaCalendario()
+        Var.calendar.setupUi(self)
+        dia = datetime.now().day
+        mes = datetime.now().month
+        ano = datetime.now().year
 
 class Main(QtWidgets.QMainWindow):
     def __init__(self):
         super(Main, self).__init__()
         Var.ui = Ui_MainWindow()
         Var.ui.setupUi(self) # encargado de generar la interfaz
+        Var.calendar = Calendar()
 
         '''
         zona de eventos de botones
         '''
-
+        Var.ui.btnCalendar.clicked.connect(Eventos.Eventos.abrirCalendar)
+        '''
+        zona de eventos del menubar
+        '''
+        Var.ui.actionSalir.triggered.connect(Eventos.Eventos.salir)
 if __name__ == "__main__": # evita que haya dos funciones iguales que se lanzen
     app = QtWidgets.QApplication([])
     window = Main()
